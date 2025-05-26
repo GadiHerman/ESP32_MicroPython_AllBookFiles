e7065c19ce954fb8d20ad0544b92bb7d00588c09
Gadi Herman <gadi.herman@gmail.com>
2025-05-26 15:55:39 +0300

    update wifi code

--- 12_WiFi/WiFi1.py
+++ 12_WiFi/WiFi1.py
@@ -1,17 +1,14 @@
-print("Scanning for WiFi networks, please wait...")
-print("")
-
-
 import network
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
 
+print("Scanning for WiFi networks, please wait...\n")
 
 authmodes = ['Open', 'WEP', 'WPA-PSK' 'WPA2-PSK4', 'WPA/WPA2-PSK']
 for (ssid, bssid, channel, RSSI, authmode, hidden) in sta_if.scan():
   print("* {:s}".format(ssid))
-  print("   - Auth: {} {}".format(authmodes[authmode], '(hidden)' if hidden else ''))
   print("   - Channel: {}".format(channel))
   print("   - RSSI: {}".format(RSSI))
   print("   - BSSID: {:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}".format(*bssid))
+  print("   - Hidden: {}".format(hidden))
   print()
